d6e667449c4ccbe2fb2bb254615b12514f4e226d
Edison Aquino de Meireles Neto <edisonamn@gmail.com>
2021-08-29 15:53:36 -0300

    Update frango.sh

--- frango.sh
+++ frango.sh
@@ -4,7 +4,7 @@ LATESTSM=$(wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/sou
 wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
 wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/"${LATESTSM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
 
-echo '"Plugin" {"file" "../csgo/addons/metamod/bin/server"}' > "${STEAMAPPDIR}/${STEAMAPP}/addons/metamod.vdf"
+wget https://github.com/Edisonamn/csgo/blob/main/metamod.vdf -P "${STEAMAPPDIR}/${STEAMAPP}/addons"
 echo "addons/sourcemod/bin/sourcemod_mm" >> "${STEAMAPPDIR}/${STEAMAPP}/addons/metamod/metaplugins.ini"
 
 wget https://github.com/Edisonamn/csgo/blob/main/frango.cfg -P "${STEAMAPPDIR}/${STEAMAPP}/cfg"
