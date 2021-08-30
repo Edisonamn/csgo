7689700d4a2725c1c48a15b5da17cf69ae96e956
Edison Aquino de Meireles Neto <edisonamn@gmail.com>
2021-08-30 00:15:45 -0300

    Update frango.sh

--- frango.sh
+++ frango.sh
@@ -1,14 +1,80 @@
 #!/bin/bash
 
-LATESTMM=$(wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/mmsource-latest-linux)
-LATESTSM=$(wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/sourcemod-latest-linux)
+mkdir -p "${STEAMAPPDIR}" || true  
 
-wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
-wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/"${LATESTSM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
+bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
+				+force_install_dir "${STEAMAPPDIR}" \
+				+app_update "${STEAMAPPID}" \
+				+quit
+        
+# We assume that if the config is missing, that this is a fresh container
+if [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg" ]; then
+	# Download & extract the config
+	wget -qO- "${DLURL}/master/etc/cfg.tar.gz" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
+	
+	# Are we in a metamod container?
+	if [ ! -z "$METAMOD_VERSION" ]; then
+		LATESTMM=$(wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/mmsource-latest-linux)
+		wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"	
+	fi
 
-rm  "${STEAMAPPDIR}/${STEAMAPP}/addons/metamod.vdf"
-wget https://github.com/Edisonamn/csgo/blob/main/metamod.vdf -P "${STEAMAPPDIR}/${STEAMAPP}/addons"
-echo "addons/sourcemod/bin/sourcemod_mm" >> "${STEAMAPPDIR}/${STEAMAPP}/addons/metamod/metaplugins.ini"
+	# Are we in a sourcemod container?
+	if [ ! -z "$SOURCEMOD_VERSION" ]; then
+		LATESTSM=$(wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/sourcemod-latest-linux)
+		wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/"${LATESTSM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
+	fi
 
-wget https://github.com/Edisonamn/csgo/blob/main/frango.cfg -P "${STEAMAPPDIR}/${STEAMAPP}/cfg"
-echo "frango.cfg" >> "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
+	# Change hostname on first launch (you can comment this out if it has done it's purpose)
+	sed -i -e 's/{{SERVER_HOSTNAME}}/'"${SRCDS_HOSTNAME}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
+  
+  rm  "${STEAMAPPDIR}/${STEAMAPP}/addons/metamod.vdf"
+  wget https://github.com/Edisonamn/csgo/blob/main/metamod.vdf -P "${STEAMAPPDIR}/${STEAMAPP}/addons"
+  echo "addons/sourcemod/bin/sourcemod_mm" >> "${STEAMAPPDIR}/${STEAMAPP}/addons/metamod/metaplugins.ini"
+
+  wget https://github.com/Edisonamn/csgo/blob/main/frango.cfg -P "${STEAMAPPDIR}/${STEAMAPP}/cfg"
+  echo "frango.cfg" >> "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
+fi
+
+autoexec_file="${STEAMAPPDIR}/${STEAMAPP}/cfg/autoexec.cfg"
+ow_args=""
+
+if [ -f "$autoexec_file" ]; then
+        # TAB delimited name    default
+        # HERE doc to not add extra file
+        while IFS=$'\t' read -r name default
+        do
+                if ! grep -q "^\s*$name" "$autoexec_file"; then
+                        ow_args="${ow_args} $default"
+                fi
+        done <<EOM
+sv_password	+sv_password "${SRCDS_PW}"
+rcon_password	+rcon_password "${SRCDS_RCONPW}"
+EOM
+
+fi
+
+cd "${STEAMAPPDIR}"
+
+bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
+			-steam_dir "${STEAMCMDDIR}" \
+			-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
+			-usercon \
+			+fps_max "${SRCDS_FPSMAX}" \
+			-tickrate "${SRCDS_TICKRATE}" \
+			-port "${SRCDS_PORT}" \
+			+tv_port "${SRCDS_TV_PORT}" \
+			+clientport "${SRCDS_CLIENT_PORT}" \
+			-maxplayers_override "${SRCDS_MAXPLAYERS}" \
+			+game_type "${SRCDS_GAMETYPE}" \
+			+game_mode "${SRCDS_GAMEMODE}" \
+			+mapgroup "${SRCDS_MAPGROUP}" \
+			+map "${SRCDS_STARTMAP}" \
+			+sv_setsteamaccount "${SRCDS_TOKEN}" \
+			+sv_region "${SRCDS_REGION}" \
+			+net_public_adr "${SRCDS_NET_PUBLIC_ADDRESS}" \
+			-ip "${SRCDS_IP}" \
+			+host_workshop_collection "${SRCDS_HOST_WORKSHOP_COLLECTION}" \
+			+workshop_start_map "${SRCDS_WORKSHOP_START_MAP}" \
+			-authkey "${SRCDS_WORKSHOP_AUTHKEY}" \
+			"${ow_args}" \
+			"${ADDITIONAL_ARGS}"
